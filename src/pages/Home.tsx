1d393dbc69f03c607d67c9f8679f80edc508fcd9
fernando predes <fernando_predes@hotmail.com>
2022-10-13 18:19:38 -0300

    usestate, useeffect e fetch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useState, useEffect } from "react";
 
-type Props = {}
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
+
+type Props = {
+
+}
 
 const Home = (props: Props) => {
+
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url: RequestInfo | URL) =>{
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+    getTopRatedMovies(topRatedUrl)
+
+  }, [])
+
+
+
   return (
     <div>
       <h1>Home</h1>
